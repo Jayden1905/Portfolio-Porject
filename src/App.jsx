aec27af7449230910564282da3a98ce350c68f38
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-10-20 00:51:04 +0800

    backend setup

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="relative h-screen w-screen">
-      <div className="side-bar h-screen w-20 bg-red-500"></div>
-    </div>
-  );
-}
-
-export default App;
