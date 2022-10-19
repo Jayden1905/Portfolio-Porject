cfa3c73ebfab8bb5d207c530bb98a2bf610af700
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-10-19 23:14:50 +0800

    Project starter

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="relative h-screen w-screen">
+      <div className="side-bar h-screen w-20 bg-red-500"></div>
+    </div>
+  );
+}
+
+export default App;
