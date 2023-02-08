076ba7e0a67cece8993698bde9ca7daed636d2c0
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-02-09 00:46:00 +0800

    change current year

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { useGlobalContext } from "../../components/Context/ContextProvider";
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useGlobalContext } from '../../components/Context/ContextProvider'
 
 const Footer = () => {
-  const { mouseSocialLinks, mouseDefault } = useGlobalContext();
+  const currentYear = new Date().getFullYear()
+  const { mouseSocialLinks, mouseDefault } = useGlobalContext()
   return (
-    <footer class="flex w-full flex-col items-center justify-center px-10 py-10 font-saira shadow sm:px-24 md:flex-row  md:justify-between">
-      <span class="text-center text-lg text-white">
-        © 2022
+    <footer class='flex w-full flex-col items-center justify-center px-10 py-10 font-saira shadow sm:px-24 md:flex-row  md:justify-between'>
+      <span class='text-center text-lg text-white'>
+        © {currentYear}
         <motion.a
           onMouseEnter={mouseSocialLinks}
           onMouseLeave={mouseDefault}
-          href="#Home"
-          class="ml-2"
-          target="_blank"
+          href='#Home'
+          class='ml-2'
+          target='_blank'
         >
           ODDINARY
         </motion.a>
         . All Rights Reserved.
       </span>
-      <ul class="mt-3 flex flex-wrap items-center justify-center text-lg text-white sm:mt-0">
+      <ul class='mt-3 flex flex-wrap items-center justify-center text-lg text-white sm:mt-0'>
         <motion.li onMouseEnter={mouseSocialLinks} onMouseLeave={mouseDefault}>
           <a
-            href="https://www.facebook.com/kyawza.yannaing.77"
-            rel="facebook noreferrer"
-            class="mr-4 md:mr-6"
-            target="_blank"
+            href='https://www.facebook.com/kyawza.yannaing.77'
+            rel='facebook noreferrer'
+            class='mr-4 md:mr-6'
+            target='_blank'
           >
             Facebook
           </a>
@@ -34,9 +35,9 @@ const Footer = () => {
           <motion.a
             onMouseEnter={mouseSocialLinks}
             onMouseLeave={mouseDefault}
-            href="https://www.instagram.com/kz_potay/?igshid=YTY2NzY3YTc%3D"
-            class="mr-4 md:mr-6"
-            target="_blank"
+            href='https://www.instagram.com/kz_potay/?igshid=YTY2NzY3YTc%3D'
+            class='mr-4 md:mr-6'
+            target='_blank'
           >
             Instagram
           </motion.a>
@@ -45,9 +46,9 @@ const Footer = () => {
           <motion.a
             onMouseEnter={mouseSocialLinks}
             onMouseLeave={mouseDefault}
-            href="https://www.youtube.com/@kyawzayannaing8007"
-            class="mr-4 md:mr-6"
-            target="_blank"
+            href='https://www.youtube.com/@kyawzayannaing8007'
+            class='mr-4 md:mr-6'
+            target='_blank'
           >
             Youtube
           </motion.a>
@@ -56,15 +57,15 @@ const Footer = () => {
           <motion.a
             onMouseEnter={mouseSocialLinks}
             onMouseLeave={mouseDefault}
-            href="https://github.com/Jayden1905"
-            target="_blank"
+            href='https://github.com/Jayden1905'
+            target='_blank'
           >
             GitHub
           </motion.a>
         </li>
       </ul>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
