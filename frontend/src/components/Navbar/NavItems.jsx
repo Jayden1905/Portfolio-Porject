f282083bcf5ad6a14bc5edd55f17530de88babf3
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-04-27 22:54:38 +0800

    nav items

--- frontend/src/components/Navbar/NavItems.jsx
+++ frontend/src/components/Navbar/NavItems.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { popUpParent, popUp } from "../../animation";
-import { useGlobalContext } from "../Context/ContextProvider";
+import React from 'react'
+import { motion } from 'framer-motion'
+import { popUpParent, popUp } from '../../animation'
+import { useGlobalContext } from '../Context/ContextProvider'
 
 export default function NavItems({ open, setOpen, setCloseDelay }) {
-  const { mouseChangeBackground, mouseDefault } = useGlobalContext();
+  const { mouseChangeBackground, mouseDefault } = useGlobalContext()
   const items = [
-    { name: "Home", hash: "#Home" },
-    { name: "About me", hash: "#About" },
-    { name: "Projects", hash: "#Works" },
-    { name: "Skills", hash: "#Skills" },
-    { name: "Contact me", hash: "#Contact" },
-  ];
+    { name: 'Home', hash: '#Home' },
+    { name: 'About me', hash: '#About' },
+    { name: 'Projects', hash: '#Works' },
+    { name: 'Skills', hash: '#Skills' },
+    { name: 'Contact me', hash: '#Contact' },
+  ]
 
   const closeNavHandler = () => {
-    setOpen((prev) => !prev);
-    open ? setCloseDelay(true) : setCloseDelay(false);
-  };
+    setOpen((prev) => !prev)
+    open ? setCloseDelay(true) : setCloseDelay(false)
+  }
 
   return (
     <motion.ul
       className={`nav-menu ${
-        open ? " " : "pointer-events-none"
+        open ? ' ' : 'pointer-events-none'
       } absolute top-[120%] sm:left-20 left-10 z-10 flex flex-col gap-4 lg:top-[150%] lg:left-[26rem] lg:gap-10`}
       variants={popUpParent}
-      initial="close"
-      animate={open ? "open" : "close"}
+      initial='close'
+      animate={open ? 'open' : 'close'}
       onMouseEnter={mouseChangeBackground}
       onMouseLeave={mouseDefault}
     >
@@ -34,9 +34,9 @@ export default function NavItems({ open, setOpen, setCloseDelay }) {
           <motion.div
             key={index}
             onClick={closeNavHandler}
-            className="h-full w-[20rem] cursor-pointer overflow-hidden transition-all duration-500 hover:pl-6"
+            className='h-full w-[20rem] cursor-pointer overflow-hidden transition-all duration-500 hover:pl-6'
           >
-            <motion.li className="transition-all duration-500" variants={popUp}>
+            <motion.li className='transition-all duration-500' variants={popUp}>
               <motion.p
                 className={`font-saira text-3xl tracking-wider lg:text-4xl ${item.id}`}
               >
@@ -44,8 +44,8 @@ export default function NavItems({ open, setOpen, setCloseDelay }) {
               </motion.p>
             </motion.li>
           </motion.div>
-        );
+        )
       })}
     </motion.ul>
-  );
+  )
 }
